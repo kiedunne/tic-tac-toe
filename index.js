9e32a9bb04e79f52682727d742d5317daa0e8114
kiedunne <kie.dunne@gmail.com>
2019-01-17 22:22:21 +0000

    can place X on grid

---
+++ index.js
@@ -0,0 +1,9 @@
+app.get('/', (req, res) => {
+  res.send('ci with travis');
+});
+
+const server = app.listen(3000, () => {
+  console.log('App running on port 3000');
+});
+
+module.exports = server;
---
+++ test/grid_spec.js
@@ -0,0 +1,30 @@
+describe("Grid", function() {
+
+  beforeEach(function() {
+    grid = new Grid();
+  });
+
+  describe('can place moves on the grid', function() {
+    it('greates a new grid with 9 empty spaces', function() {
+        expect(grid.grid.length).toEqual(9)
+    });
+
+    it('can place an X at a position', function() {
+        grid.move(1)
+        expect(grid.grid).toEqual(['','X','','','','','','',''])
+    });
+
+    it('cannot place an X at if it already has a symbol', function() {
+        grid.move(1)
+        expect(function() {
+          grid.move(1);
+        }).toThrowError('Spot taken!');
+    });
+
+    it('cannot place an X at if it already has a symbol', function() {
+        expect(function() {
+          grid.move("1");
+        }).toThrowError('Must be a number.');
+      });
+    });
+});
